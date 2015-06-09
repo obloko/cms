666bccffc520ea63f7853fe444a2258517bbc1cc
Matt Shaw <matt@mattshaw.org>
2015-06-09 12:31:48 -0400

    .

--- manage.py
+++ manage.py
@@ -4,8 +4,6 @@ import os, sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cms.settings")
-    print "os.path.abspath(''):",os.path.abspath('')
-    print "os.path.join(os.path.abspath(''),'modules'):",os.path.join(os.path.abspath(''),'modules')
     os.path.sys.path.append(os.path.join(os.path.abspath(''),'modules'))
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
